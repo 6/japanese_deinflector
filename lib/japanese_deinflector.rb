68b9e2f925adcb9ecbad2a98e011a4da7bfc64a0
Peter Graham <peter@1000memories.com>
2012-12-07 18:42:29 -0700

    Refactor deinflect and rule filtering methods to be more readable

--- lib/japanese_deinflector.rb
+++ lib/japanese_deinflector.rb
@@ -6,34 +6,51 @@ require "japanese_deinflector/version"
 class JapaneseDeinflector
   def initialize
     File.open(File.join(File.expand_path(File.dirname(__FILE__)), 'data/deinflect.json')) do |f|
-      rules = JSON.parse(f.read, :symbolize_names => true)
+      rule_groups = JSON.parse(f.read, :symbolize_names => true)
       # Convert hash keys to integers (from something like :"9" -> 9)
-      @rules = {}
-      rules.each do |size, rules|
-        @rules[size.to_s.to_i] = rules
+      @rule_groups = {}
+      rule_groups.each do |suffix_size, rules|
+        @rule_groups[suffix_size.to_s.to_i] = rules
       end
     end
   end
 
   def deinflect(word)
-    possibilities = []
-    rules_less_than_size(word.size).each do |size, rules|
-      ending = word[-size..-1]
-      rules.each do |rule|
-        next  unless ending == rule[:from]
-        deinflected_word = "#{word[0..-size-1]}#{rule[:to]}"
-        next  if possibilities.include?(deinflected_word)
-        # Weight is between 0 and 1, 1 being a higher chance of actual deinflection
-        weight = (Float(size) / word.size).round(3)
-        possibilities << {:weight => weight, :word => deinflected_word, :reason => rule[:reason]}
+    results = Set.new
+
+    filter_rule_groups(@rule_groups, :max_suffix_size => word.size).each do |suffix_size, rules|
+      from_suffix = word[-suffix_size..-1]
+      filter_rules(rules, :from_suffix => from_suffix).each do |rule|
+        results << result_hash(word, from_suffix, rule[:to], rule[:reason])
       end
     end
-    possibilities
+
+    results.to_a
   end
 
   private
 
-  def rules_less_than_size(max_size)
-    @rules.clone.delete_if{|size, rules| size >= max_size}
+  def result_hash(word, from_suffix, to_suffix, reason = "")
+    # Append new suffix to the original word to get the deinflection
+    deinflected_word = "#{word[0..-from_suffix.size-1]}#{to_suffix}"
+    # Weight is between 0 and 1, 1 being a higher chance of correct deinflection
+    weight = (Float(from_suffix.size) / word.size).round(3)
+    {
+      :word => deinflected_word,
+      :weight => weight,
+      :reason => reason,
+    }
+  end
+
+  def filter_rule_groups(groups, filters = {})
+    groups.clone.delete_if do |suffix_size, rules|
+      suffix_size >= filters[:max_suffix_size]  if filters[:max_suffix_size]
+    end
+  end
+
+  def filter_rules(rules, filters = {})
+    rules.clone.delete_if.each do |rule|
+      rule[:from] != filters[:from_suffix]  if filters[:from_suffix]
+    end
   end
 end
